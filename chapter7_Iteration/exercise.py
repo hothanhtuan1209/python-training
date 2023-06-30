fbae297bdb7081b91100e6559eb0ccf388be360f
Tuan Ho <hothanhtuan1209@gmai.com>
2023-06-30 09:55:02 +0700

    Add exercise 7-3

--- chapter7_Iteration/exercise.py
+++ chapter7_Iteration/exercise.py
@@ -1,10 +1,13 @@
-#Exercise 7-1
+import math
+
+# Exercise 7-1
 def mysqrt(a):
     x = a/2
     while True:
         y = (x + a/x) / 2
         return y 
-import math
+
+
 def math_sqrt():
     print('a     mysqrt(a)     math.sqrt(a)    diff')
     print('-     ----------   -------------    -----')
@@ -17,7 +20,8 @@ def math_sqrt():
         a+=1
 math_sqrt()        
 
-#Exercise 7-2:
+
+# Exercise 7-2:
 def eval_loop():
     while True:
         input_values = input('enter an expression or done to finish: ')
@@ -27,3 +31,33 @@ def eval_loop():
             result = eval(input_values)
             print(result)
 eval_loop()
+
+
+# Exercise 7-3:
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        recurse = factorial(n-1)
+        result = n * recurse
+        return result
+
+
+def estimate_pi():
+    total = 0
+    k = 0
+    factor = 2 * math.sqrt(2) / 9801
+    while True:
+        num = factorial(4*k) * (1103 + 26390*k)
+        den = factorial(k)**4 * 396**(4*k)
+        
+        total += num / den
+        term = factor * num/den
+        
+        if abs(term) < 1e-15:
+            break
+        k += 1
+    
+    return 1 / (factor * total)
+print(estimate_pi())
+print(math.pi)
