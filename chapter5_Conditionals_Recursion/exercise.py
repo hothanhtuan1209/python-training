5e31387cc9b3aafd37090e0dea3090b73faf3833
Tuan Ho <hothanhtuan1209@gmai.com>
2023-07-03 08:29:22 +0700

    Fix left margin

--- chapter5_Conditionals_Recursion/exercise.py
+++ chapter5_Conditionals_Recursion/exercise.py
@@ -30,19 +30,19 @@ to see if Fermat’s theorem holds. If n is greater than 2 and the program shoul
 Fermat was wrong!” Otherwise the program should print, “No, that doesn’t work.” """
 
 def check_fermat(a,b,c,n):
-        if n>2 and (a**n + b**n == c**n):
-            print('Holy smokes, Fermat was wrong')
-        else:
-            print("no, that doesn't work")
+    if n>2 and (a**n + b**n == c**n):
+        print('Holy smokes, Fermat was wrong')
+    else:
+        print("no, that doesn't work")
 check_fermat(4,5,7,3)
 
 """2. Write a function that prompts the user to input values for a, b, c and n, converts them to integers,
  and uses check_fermat to check whether they violate Fermat’s theorem"""
 def check_fermat(a,b,c,n):
-        if n>2 and (a**n + b**n == c**n):
-            print('Holy smokes, Fermat was wrong')
-        else:
-            print("no, that doesn't work")
+    if n>2 and (a**n + b**n == c**n):
+        print('Holy smokes, Fermat was wrong')
+    else:
+        print("no, that doesn't work")
 def input_check_fermat():
     a = int(input('a='))
     b = int(input('b='))
