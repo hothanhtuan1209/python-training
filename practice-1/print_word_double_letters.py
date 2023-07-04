bd4a39b0ce8a8d79e56e3cfec9afb1b1fa532788
Tuan Ho <hothanhtuan1209@gmai.com>
2023-07-04 16:54:06 +0700

    Add file excercise double letter

---
+++ practice-1/print_word_double_letters.py
@@ -0,0 +1,24 @@
+def is_triple_double(word):
+    i = 0
+    count = 0
+    while i < len(word)-1:
+        if word[i] == word[i+1]:
+            count += 1
+            if count == 3:
+                return True
+            i +=2
+        else:
+            i = i + 1 - 2*count
+            count = 0
+    return False
+
+def find_triple_double():
+    """Reads a word list and prints words with triple double letters."""
+    fin = open('D:/forGit/training_python\practice-1/words.txt')
+    for line in fin:
+        word = line.strip()
+        if is_triple_double(word):
+            print(word)
+
+print('three consecutive double letters i:s')
+find_triple_double()
