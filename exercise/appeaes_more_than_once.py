92663c009f0357c13ff6f02237eb7df13ba47987
Tuan Ho <hothanhtuan1209@gmai.com>
2023-07-10 11:23:49 +0700

    Move docstring below create function line

--- exercise/appeaes_more_than_once.py
+++ exercise/appeaes_more_than_once.py
@@ -8,13 +8,14 @@ any element that appears more than once.
 """
 
 def has_duplicates():
-    list_input = list(input('Enter list:'))
-    set_list_input = set(set_list_input)
     """
     Input a list 
 
     Return True if list has any element appears more than once.
     """
+    
+    list_input = list(input('Enter list:'))
+    set_list_input = set(set_list_input)
 
     if len(list_input) == len(set_list_input):
         return False
