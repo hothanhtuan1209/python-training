80cc276f7fa4e21bd4843a068c41327e0702e39f
Tuan Ho <hothanhtuan1209@gmai.com>
2023-07-10 14:40:38 +0700

    Fix docstring, fix url file word.txt

--- exercise/two_function_add_word.py
+++ exercise/two_function_add_word.py
@@ -6,17 +6,19 @@ Chapter 10: Lists
 Reads the file words.txt and builds a list with one element per
 word. Write two versions of this function, one using the append
 method and the other using the idiom t = t + [x].
+
+Download the word.txt file before running the program 
 """
 
 def word_list_append():
     """"
-    Enter a word list
+    Take word list from file word.txt 
 
-    return: create new word list using 'append' to add word from flie 
+    return: new word list using 'append' to add word from flie 
     """
-    
+
     word_list = []
-    read_file = open('D:/forGit/training_python\practice-1/words.txt')
+    read_file = open('words.txt')
 
     for line in read_file:
         word = line.strip()
@@ -28,13 +30,13 @@ word_list_append()
 
 def word_list_concatenate():
     """"
-    Enter a word list
+    Take word list from file word.txt 
 
-    return: create new word list using the idiom t = t + [x] to add word from file
+    return: new word list using the idiom word_list = word_list + [word] to add word from file
     """
 
     word_list = []
-    read_file = open('D:/forGit/training_python\practice-1/words.txt')
+    read_file = open('words.txt')
 
     for line in read_file:
         word = line.strip()
