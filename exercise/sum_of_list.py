f838912a82b635d4cf0daa89f9bed84244238b16
Tuan Ho <hothanhtuan1209@gmai.com>
2023-07-11 10:12:58 +0700

    Fix docstring

--- exercise/sum_of_list.py
+++ exercise/sum_of_list.py
@@ -11,11 +11,9 @@ list_input = [[1, 2], [3], [4, 5, 6]]
 
 def nested_sum(list_input):
     """
-    This function sums the elements in the list, including the list in it
+    Sum the elements in the list, including the nested list
     
-    Take in a list of numbers or list nested lists
-
-    return:  calculate total all number in list 
+    print: int 
     """
 
     total = 0 
