568ad37ff825f39dce5124dd27209682b189dea3
Tuan Ho <hothanhtuan1209@gmai.com>
2023-07-31 09:19:14 +0700

    Add exercise house

---
+++ exercise/house.py
@@ -0,0 +1,11 @@
+"""
+Create class House and two attribute address, street
+"""
+
+class House:
+    def __init__(self, address, street):
+        self.address = address
+        self.street = street
+    
+house_1 = House(55, 'Nui Thanh')
+house_2 = House(56, 'Nui Thanh')
