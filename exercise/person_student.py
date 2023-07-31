6e8531154ea2fd9f51c670756e957f5ea991c533
Tuan Ho <hothanhtuan1209@gmai.com>
2023-07-31 09:16:05 +0700

    Add exercise person_student

---
+++ exercise/person_student.py
@@ -0,0 +1,26 @@
+"""
+Create class Person and class Student, class Student inherrits from person class
+and adds score attribute, compare scores of 2 students
+"""
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Student(Person):
+    def __init__(self, name, age, score):
+        super().__init__(name, age)
+
+        self.score = score
+
+def compare_score():
+    if student_1.score > student_2.score:
+        return True    
+    else:
+        return False
+    
+student_1 = Student('Nam', 6, 7)
+student_2 = Student('Hai', 10, 5)
+
+print(compare_score())
