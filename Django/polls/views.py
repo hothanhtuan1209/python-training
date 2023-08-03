483f74a31326e8a30c15efd2877064a7378ffd81
Tuan Ho <hothanhtuan1209@gmai.com>
2023-08-03 09:48:51 +0700

    Remove index, detail and results views and add generic views

--- Django/polls/views.py
+++ Django/polls/views.py
@@ -2,33 +2,27 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from django.http import Http404
+from django.views import generic
 
 from .models import Question
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-    return HttpResponse(template.render(context, request))
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    
-    except Question.DoesNotExist:
-        raise Http404("Question do not exist")
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-    return render(request, "polls/detail.html", {"question":question})
-
-def results(request, question_id):
-    response = "You're looking at the results of the question %s."
-    
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." %question_id)
