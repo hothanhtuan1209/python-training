2c8ee6fc7cfa0a6b0558a8a8b1f302f6ac2b1152
Tuan Ho <hothanhtuan1209@gmai.com>
2023-08-03 17:46:51 +0700

    Update docstring for file apps.py

--- Django/polls/apps.py
+++ Django/polls/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
 
 class PollsConfig(AppConfig):
+    """
+    Configuration class for the 'polls' app.
+
+    Attributes:
+        default_auto_field (str): The name of the default auto-generated primary key field.
+        name (str): The name of the app ('polls' in this case).
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
