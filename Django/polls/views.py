762003512fa9d84d0a7ab26ffcef9b3962faace4
Tuan Ho <hothanhtuan1209@gmai.com>
2023-08-07 09:02:03 +0700

    Updates vote function in polls/views

--- Django/polls/views.py
+++ Django/polls/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.utils import timezone
 from django.db.models import QuerySet
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
 
-from .models import Question
+from .models import Question, Choice
 
 
 class IndexView(generic.ListView):
@@ -18,10 +20,15 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
-    def get_queryset(self) -> QuerySet[Question]:
-        """Return the last five published questions."""
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
 
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DetailView):
@@ -72,7 +79,22 @@ def vote(request, question_id):
         HttpResponse: The HTTP response.
     """
 
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 
 def get_queryset():
