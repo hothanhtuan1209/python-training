9c562341bdfe957951818f7461fbcd76624ad79a
Tuan <hothanhtuan1209@gmail.com>
2023-10-12 17:24:36 +0700

    Update docstring for function in print_word_avoids_letter.py

--- practice-1/print_word_double_letters.py
+++ practice-1/print_word_double_letters.py
@@ -4,7 +4,17 @@ This lesson prints words containing three consecutive double letters
 
 
 def is_triple_double(word):
-    # Find three consecutive double letters
+    """
+    Check if a word contains three consecutive double letters.
+
+    Parameters:
+        - word (str): The input word to check.
+
+    Returns:
+        - bool: True if the word contains three consecutive double letters,
+        False otherwise.
+    """
+
     i = 0
     count = 0
 
@@ -23,7 +33,10 @@ def is_triple_double(word):
 
 
 def find_triple_double():
-    # Reads a word list and prints words with triple double letters.
+    """
+     Reads a word list from a file and prints words with triple double letters.
+    """
+
     fin = open('D:/forGit/training_python\practice-1/words.txt')
 
     for line in fin:
