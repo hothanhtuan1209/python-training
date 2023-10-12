5c61d33e2c5c979311cedd1b4e4965ab13a19618
Tuan <hothanhtuan1209@gmail.com>
2023-10-12 16:51:04 +0700

    Update docstring for function in count_word_abecedarian.py

--- practice-1/count_word_abecedarian.py
+++ practice-1/count_word_abecedarian.py
@@ -1,12 +1,22 @@
 """
-Write a function called is_abecedarian that returns True if the letters in a word
-appear in alphabetical order (double letters are okay). How many abecedarian words
-are there?
+Write a function called is_abecedarian that returns True if the letters in a
+word
+appear in alphabetical order (double letters are okay). How many abecedarian
+words are there?
 """
 
 
 def is_abecedarian(word):
-    # Check alphabetical order
+    """
+    Check if a word is in alphabetical order.
+
+    Parameters:
+        word (str): The word to check for alphabetical order.
+
+    Returns:
+        bool: True if the word is in alphabetical order, False otherwise.
+    """
+
     index = 0
 
     while index < len(word) - 1:
@@ -25,4 +35,4 @@ for word in list_word:
     if is_abecedarian(word):
         count += 1
 
-print('Abecedarian words:', count)  
+print('Abecedarian words:', count)
