907d3adba322d528c61df438794f3f463431a1fb
Tuan <hothanhtuan1209@gmail.com>
2023-10-19 11:11:47 +0700

    Update folder and file structure for folder django

---
+++ Django/poll_app/polls/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+
+class Question(models.Model):
+    """
+    Represents a question in the poll app.
+
+    Attributes:
+        question_text (str): The text of the question.
+        pub_date (datetime): The date and time when the question was published.
+    """
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        """
+        Returns a string representation of the Question object.
+        """
+
+        if isinstance(self.question_text, str):
+            return self.question_text
+        else:
+            return str(self.question_text)
+
+    def was_published_recently(self):
+        """
+        Returns True if the Question was published within the last 1 day;
+        otherwise, returns False.
+        """
+
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+class Choice(models.Model):
+    """
+    Represents a choice for a question in the poll app.
+
+    Attributes:
+        question (Question): The question to which this choice belongs.
+        choice_text (str): The text of the choice.
+        votes (int): The number of votes received for this choice.
+    """
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Choice object.
+        """
+
+        if isinstance(self.choice_text, str):
+            return self.choice_text
+        else:
+            return str(self.choice_text)
---
+++ Django/poll_app/polls/views.py
@@ -0,0 +1,97 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.utils import timezone
+from django.db.models import QuerySet
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    """
+    View for displaying the list of the latest questions.
+
+    Attributes:
+        template_name (str): The name of the template to be rendered.
+        context_object_name (str): The name of the context variable to store the list of questions.
+    """
+
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
+
+
+class DetailView(generic.DetailView):
+    """
+    View for displaying details of a specific question.
+
+    Attributes:
+        model (Model): The model used for the detail view.
+        template_name (str): The name of the template to be rendered.
+    """
+
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Return the queryset for the detail view.
+
+        Returns:
+            QuerySet[Question]: The queryset containing the questions that are published.
+        """
+
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    """
+    View for displaying results of a specific question.
+
+    Attributes:
+        model (Model): The model used for the detail view.
+        template_name (str): The name of the template to be rendered.
+    """
+
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request, question_id):
+    """
+    View for voting on a specific question.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+        question_id (int): The ID of the question to vote on.
+
+    Returns:
+        HttpResponse: The HTTP response.
+    """
+
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
