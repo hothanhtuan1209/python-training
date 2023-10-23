e8156802f140a59c42aff84f8d862d37b9384cb7
Tuan <hothanhtuan1209@gmail.com>
2023-10-23 17:38:24 +0700

    Use Flake8, Pyint, Black to update format code for word_frequency_report.py

--- python/practices/practice-2/src/word_frequency_report.py
+++ python/practices/practice-2/src/word_frequency_report.py
@@ -4,23 +4,24 @@ Think Python, 2nd Edition
 Chapter 13: Case Study: Data Structure Selection
 
 Modify the program to count the total number of words in the book, and the
-number of times each word is used. Print the number of different words used in the book.
-
-Download ebook.txt file
+number of times each word is used. Print the number of different words used in
+the book.
 """
 
+
 import string
+
 
 def clean_word():
     """
     Read a file and count its word.
 
-    returns: 
-        - list words: list 
+    returns:
+        - list words: list
         - count: int
     """
 
-    fin = open('ebook.txt', encoding = 'utf-8')
+    fin = open("ebook.txt", encoding="utf-8")
     cleaned_words = []
     count = 0
 
@@ -29,15 +30,16 @@ def clean_word():
 
         for word in line:
             cleaned_word = ""
-            
+
             for letter in word:
                 if letter not in string.whitespace and letter not in string.punctuation:
                     cleaned_word += letter.lower()
-            
+
             cleaned_words.append(cleaned_word)
             count += 1
-    
+
     return cleaned_words, count
+
 
 def count_word_diff(cleaned_words):
     """
@@ -45,20 +47,21 @@ def count_word_diff(cleaned_words):
 
     cleaned_words: list
 
-    return: 
+    return:
         - word_in_book: list
         - word_diff: int
     """
-    
+
     word_in_book = []
     word_diff = 0
-    
+
     for word in cleaned_words:
         if word not in word_in_book:
             word_in_book.append(word)
             word_diff += 1
-    
-    return word_in_book,word_diff
+
+    return word_in_book, word_diff
+
 
 def count_word_used(cleaned_words):
     """
@@ -68,18 +71,21 @@ def count_word_used(cleaned_words):
 
     return: dict
     """
-    
+
     count_each_word = {}
 
     for word in cleaned_words:
-        if word in count_each_word : 
+        if word in count_each_word:
             count_each_word[word] += 1
         else:
             count_each_word[word] = 1
-    
-    sorted_count_each_word = dict(sorted(count_each_word.items(), key = lambda x: x[1], reverse = True)) 
-    
+
+    sorted_count_each_word = dict(
+        sorted(count_each_word.items(), key=lambda x: x[1], reverse=True)
+    )
+
     return sorted_count_each_word
+
 
 cleaned_words, count = clean_word()
 word_in_book = count_word_diff(cleaned_words, count)
