4cfda2377caa5e7c8a0df70fd91f8b5c996d4a8f
Tuan <hothanhtuan1209@gmail.com>
2023-10-24 11:46:18 +0700

    Update idiomatic code and docstring for triple_double_letters.py file

--- python/practices/practice-1/src/triple_double_letters.py
+++ python/practices/practice-1/src/triple_double_letters.py
@@ -1,32 +1,47 @@
 """
 This lesson prints words containing three consecutive double letters
 """
+
+
 def is_triple_double(word):
-    # Find three consecutive double letters
+    """
+    Check if a word contains three consecutive double letters.
+
+    Parameters:
+        - word (str): The input word to check.
+
+    Returns:
+        - bool: True if the word contains three consecutive double letters,
+        False otherwise.
+    """
+
     i = 0
-    count = 0
-    
-    while i < len(word)-1:
-        # Check through 2 characters next to each other
-        if word[i] == word[i+1]:
-            count += 1
-            if count == 3:
-                return True
-            i +=2
-        else:
-            i = i + 1 - 2*count
-            count = 0
-    
+
+    while i < len(word) - 5:
+        if (
+            word[i] == word[i + 1]
+            and word[i + 2] == word[i + 3]
+            and word[i + 4] == word[i + 5]
+        ):
+            return True
+        i += 1
+
     return False
 
+
 def find_triple_double():
-    # Reads a word list and prints words with triple double letters.
-    fin = open('D:/forGit/training_python\practice-1/words.txt')
-    
+    """
+    Reads a word list from a file and prints words with triple double letters.
+    """
+
+    fin = open("words.txt")
+
     for line in fin:
         word = line.strip()
+
         if is_triple_double(word):
             print(word)
 
-print('three consecutive double letters is: ')
+
+print("three consecutive double letters is: ")
 find_triple_double()
