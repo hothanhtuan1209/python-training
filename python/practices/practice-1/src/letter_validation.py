3b41245937b55fc52036c9e91651b25c37207e57
Tuan <hothanhtuan1209@gmail.com>
2023-10-24 11:18:04 +0700

    Update idiomatic code and docstring for letter_validation.py file

--- python/practices/practice-1/src/letter_validation.py
+++ python/practices/practice-1/src/letter_validation.py
@@ -2,12 +2,26 @@
 Write a function named uses_only that takes a word and a string of letters,
 and that returns True if the word contains only letters in the list
 """
+
+
 def used_only(word, string_letter):
-    # Check word contains only letters 
+    """
+    Check if a word contains only the specified letters.
+
+    Parameters:
+        - word (str): The word to check.
+        - string_letter (str): A string containing the allowed letters.
+
+    Returns:
+        - bool: True if 'word' contains only letters from 'string_letter',
+        False otherwise.
+    """
+
     for letter in word:
         if letter not in string_letter:
             return False
-    
+
     return True
 
-print(used_only('hello','leo'))
+
+print(used_only('hello', 'leo'))
