6963140c9d08a2f2a3389993f49aa5bf7df6ee0c
Tuan <hothanhtuan1209@gmail.com>
2023-10-24 11:24:05 +0700

    Update idiomatic code and docstring for long_words_printer.py

--- python/practices/practice-1/src/long_words_printer.py
+++ python/practices/practice-1/src/long_words_printer.py
@@ -1,13 +1,23 @@
 """
-Write a program that reads words.txt and prints only the words with more than 20
-characters (not counting whitespace).
+Write a program that reads words.txt and prints only the words with more than
+20 characters (not counting whitespace).
 """
+
+
 def print_words(file):
+    """
+    Print words from a file that have a length greater than 20 characters.
+
+    Args:
+        - file (str): The path to the input file containing a list of words.
+    """
+
     fin = open(file)
-    
+
     for line in fin:
         word = line.strip()
         if len(word) > 20:
             print(word)
 
-print_words('D:/forGit/training_python\chapter9_practice1/words.txt')
+
+print_words('words.txt')
