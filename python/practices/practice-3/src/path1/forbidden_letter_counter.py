5e5f7c3dd00d2cd1b5151ab4a1603b99e42ae23f
Tuan <hothanhtuan1209@gmail.com>
2023-10-31 00:02:44 +0700

    Use flake8 and black to update idiomatic code for forbidden_letter_counter.py file

---
+++ python/practices/practice-3/src/path1/forbidden_letter_counter.py
@@ -0,0 +1,40 @@
+"""
+1. Write a function named avoids that takes a word and a string of forbidden
+letters, and that returns True if the word doesn't use any of the forbidden
+letters.
+2. Modify your program to prompt the user to enter a string of forbidden
+letters and
+then print the number of words that don't contain any of them. Can you find a
+combination of five forbidden letters that excludes the smallest number of
+words?
+
+def avoids():
+    words = 'hello@'
+    forbidden_letter = '!@#$%*&'
+    for letter in forbidden_letter:
+        if letter in words:
+            return False
+    return True
+results = avoids()
+print(results)
+"""
+
+
+def avoids():
+    """
+    Count the total number of words that do not contain any of the forbidden
+    letters.
+    """
+
+    words = input('Enter words separated by space:').split()
+    letters = input('Enter a string of forbidden letters:')
+
+    for total, word in enumerate(words):
+        # Does each word in word contain each letter in letters?
+        if any(letter in word for letter in letters):
+            continue
+
+    print(total)
+
+
+avoids()
