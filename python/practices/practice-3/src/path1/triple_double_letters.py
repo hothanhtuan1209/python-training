e689be08b99af3d9ceb5a72c887974537d241c1b
Tuan <hothanhtuan1209@gmail.com>
2023-10-31 09:43:34 +0700

    Use flake8 and black to update format and logic code for triple_double_letters.py file

---
+++ python/practices/practice-3/src/path1/triple_double_letters.py
@@ -0,0 +1,47 @@
+"""
+This lesson prints words containing three consecutive double letters
+"""
+
+
+def is_triple_double(word):
+    """
+    Check if a word contains three consecutive double letters.
+
+    Parameters:
+        - word (str): The input word to check.
+
+    Returns:
+        - bool: True if the word contains three consecutive double letters,
+        False otherwise.
+    """
+
+    i = 0
+
+    while i < len(word) - 5:
+        if (
+            word[i] == word[i + 1]
+            and word[i + 2] == word[i + 3]
+            and word[i + 4] == word[i + 5]
+        ):
+            return True
+        i += 1
+
+    return False
+
+
+def find_triple_double():
+    """
+     Reads a word list from a file and prints words with triple double letters.
+    """
+
+    fin = open('words.txt', encoding='utf-8')
+
+    for line in fin:
+        word = line.strip()
+
+        if is_triple_double(word):
+            print(word)
+
+
+print('three consecutive double letters is: ')
+find_triple_double()
