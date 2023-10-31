93c3035206cc29d7f12d56b0989a69e209855702
Tuan <hothanhtuan1209@gmail.com>
2023-10-31 09:45:25 +0700

    Use flake8 and black to update format and logic code for word_e_checker.py file

---
+++ python/practices/practice-3/src/path1/word_e_checker.py
@@ -0,0 +1,49 @@
+"""
+1. Write a function called has_no_e that returns True if the given word doesn't
+have the letter “e” in it.
+2. Modify your program from the previous section to print only the words that
+have no “e” and compute the percentage of the words in the list that have
+no “e”.
+"""
+
+
+def has_no_e():
+    """
+    Check if a word or string contains the letter 'e' or 'E'.
+
+    Returns:
+        - bool: True if there is no 'e' or 'E' in the word, False otherwise.
+    """
+    word = input('Enter a word or string:')
+
+    for letter in word:
+        if letter in ('e', 'E'):
+            return False
+
+    return True
+
+
+result = has_no_e()
+print(result)
+
+
+def count_words():
+    """
+    Count and calculate the percentage of words that do not contain 'E' or 'e'
+    in a list of words.
+    """
+
+    words = input("Enter the list of words: ").split()
+    count_word = len(words)
+    words_has_no_e = 0
+
+    for word in words:
+        if 'e' not in word and 'E' not in word:
+            print(word)
+            words_has_no_e += 1
+
+    percentage = (words_has_no_e/count_word) * 100
+    print('Percentage of words without the letter e', percentage)
+
+
+count_words()
