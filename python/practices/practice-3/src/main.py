95ebba620b1488d3bc127a09d93341ec74117c27
Tuan <hothanhtuan1209@gmail.com>
2023-11-01 10:27:42 +0700

    Update docstirng for modules in main.py file

--- python/practices/practice-3/src/main.py
+++ python/practices/practice-3/src/main.py
@@ -1,6 +1,23 @@
+"""
+This is the main entry point for the program.
+It calls the main function from modules.
+
+Modules:
+    - age_condition_checker: Module for checking age conditions and finding
+    pairs of ages satisfying the condition.
+"""
+
+
 from path1 import age_condition_checker
 
-list_file = [age_condition_checker]
 
-for file in list_file:
+def main():
+    """
+    To run all files code in practice-3 through main.py file
+    """
+
     age_condition_checker.main()
+
+
+if __name__ == "__main__":
+    main()
