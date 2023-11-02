3557100cc6c542a47b3c6c13b250a38a8121a7a2
Tuan <hothanhtuan1209@gmail.com>
2023-11-02 09:00:13 +0700

    Refactor abecedarian_word_counter.py file, update function main

---
+++ python/practices/practice-3/src/path1/abecedarian_word_counter.py
@@ -0,0 +1,42 @@
+"""
+Check the list of words to see if for each word the letters inside are in
+alphabetical order
+"""
+
+
+import os
+
+
+def check_abecedarian(word):
+    """
+    Check if a word is in alphabetical order.
+
+    Parameters:
+        word (str): The word to check for alphabetical order.
+
+    Returns:
+        bool: True if the word is in alphabetical order, False otherwise.
+    """
+
+    for i in range(len(word) - 1):
+        if word[i] > word[i + 1]:
+            return False
+
+    return True
+
+
+def main():
+    """
+    This function calls function check_abecedarian to check a list of words
+    and print words that satisfy the condition
+    """
+
+    list_word = input('Enter a list word:').split()
+    count = 0
+
+    for word in list_word:
+        if check_abecedarian(word):
+            count += 1
+
+    print(os.path.basename(__file__))
+    print('Abecedarian words:', count)
