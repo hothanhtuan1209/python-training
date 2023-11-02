c5f09c643e7c23302e71a3ce3458ee4cbc6937df
Tuan <hothanhtuan1209@gmail.com>
2023-11-03 00:16:30 +0700

    Update module triple_double_letters for main.py

--- python/practices/practice-3/src/main.py
+++ python/practices/practice-3/src/main.py
@@ -7,14 +7,16 @@ It calls the main function from modules.
 from path1 import (
     age_condition_checker,
     abecedarian_word_counter,
-    forbidden_letter_counter
+    forbidden_letter_counter,
+    triple_double_letters
 )
 
 
 modules = [
     age_condition_checker,
     abecedarian_word_counter,
-    forbidden_letter_counter
+    forbidden_letter_counter,
+    triple_double_letters
 ]
 
 
