80524cb2e4c68688a2474fae1a4baafa208e55b6
Tuan <hothanhtuan1209@gmail.com>
2023-11-03 00:43:11 +0700

    Update file word_e_checker.py for practice-3

---
+++ python/practices/practice-3/src/path1/word_e_checker.py
@@ -0,0 +1,35 @@
+"""
+Count and print percentage word in word list not contains the letter 'e' or 'E'
+"""
+
+
+import os
+
+
+def count_words():
+    """
+    Count and calculate the percentage of words that do not contain 'E' or 'e'
+    in a list of words.
+    """
+
+    words = input("Enter the list of words: ").split()
+    count_word = len(words)
+    words_has_no_e = 0
+
+    for word in words:
+        if 'e' not in word and 'E' not in word:
+            print(word)
+            words_has_no_e += 1
+
+    percentage = (words_has_no_e/count_word) * 100
+    print('Percentage of words without the letter e', percentage)
+
+
+def main():
+    """
+    Count and print percentage word in word list not contains the
+    letter 'e' or 'E'
+    """
+
+    print(os.path.basename(__file__))
+    count_words()
