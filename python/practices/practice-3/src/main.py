366965c851544a2589f68bc977fe921741db5d2a
Tuan <hothanhtuan1209@gmail.com>
2023-11-02 14:01:19 +0700

    Update file main.pt, add forbidden_letter_counter module to main.py

--- python/practices/practice-3/src/main.py
+++ python/practices/practice-3/src/main.py
@@ -4,7 +4,14 @@ It calls the main function from modules.
 """
 
 
-from path1 import age_condition_checker
+from path1 import (
+    forbidden_letter_counter,
+)
+
+
+modules = [
+    forbidden_letter_counter
+]
 
 
 def main():
@@ -12,7 +19,8 @@ def main():
     To run all files code in practice-3 through main.py file
     """
 
-    age_condition_checker.main()
+    for module in modules:
+        module.main()
 
 
 if __name__ == "__main__":
