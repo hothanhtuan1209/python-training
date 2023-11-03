783f32289da4ca9635dd2c3eb74d86ca53cf7218
Tuan <hothanhtuan1209@gmail.com>
2023-11-03 22:06:39 +0700

    Add empty line for file_reader.py

--- python/practices/practice-3/src/helpers/file_reader.py
+++ python/practices/practice-3/src/helpers/file_reader.py
@@ -18,10 +18,15 @@ def read_file_content(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
+
         return content
+
     except FileNotFoundError:
         print(f"File '{file_path}' not found.")
+
         return None
+
     except Exception as e:
         print(f"An error occurred while reading the file: {e}")
+
         return None
