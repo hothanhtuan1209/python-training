bcbcf053c46bc8993f1668668098e5ce2d5a8ee6
Tuan <hothanhtuan1209@gmail.com>
2023-11-04 07:40:43 +0700

    Create new function to calculate percentage words not contain letter e

--- python/practices/practice-3/src/path1/word_e_checker.py
+++ python/practices/practice-3/src/path1/word_e_checker.py
@@ -10,9 +10,11 @@ def count_words(words):
     """
     Count and calculate the percentage of words that do not contain 'E' or 'e'
     in a list of words.
+
+    Parameters:
+        - words: list of words input
     """
 
-    total_number_of_words = len(words)
     words_has_no_e = 0
 
     for word in words:
@@ -20,6 +22,20 @@ def count_words(words):
             print(word)
             words_has_no_e += 1
 
+    return words_has_no_e
+
+
+def percentage_words(words, words_has_no_e):
+    """
+    Calculate the percentage of words that do not contain 'E' or 'e'
+    in a list of words
+
+    Parameters:
+        - words: list of words input
+        - words_has_no_e: list of words not contain letter 'e' or 'E'
+    """
+
+    total_number_of_words = len(words)
     percentage = (words_has_no_e/total_number_of_words) * 100
     print('Percentage of words without the letter e', percentage)
 
