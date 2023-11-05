6d9d06cda4260f1e5e39e9f5b9fbf0e899e36e5b
Tuan <hothanhtuan1209@gmail.com>
2023-11-05 23:13:48 +0700

    Update command import constant file

--- python/practices/practice-3/src/path1/long_words_printer.py
+++ python/practices/practice-3/src/path1/long_words_printer.py
@@ -6,7 +6,7 @@ than 20 letters excluding spaces
 
 import os
 from helpers.file_reader import read_file
-from constants.constant import word_file
+from constants.constants import path_of_word_file
 
 
 def print_words(file_content):
@@ -31,7 +31,7 @@ def main():
     """
 
     print(os.path.basename(__file__))
-    file_content = read_file(word_file)
+    file_content = read_file(path_of_word_file)
 
     if file_content:
         print_words(file_content)
