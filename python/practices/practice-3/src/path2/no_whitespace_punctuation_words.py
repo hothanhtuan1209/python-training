1fc2ce0ca6af1df6d9ce3d7992048c366280f523
Tuan <hothanhtuan1209@gmail.com>
2023-11-06 00:12:13 +0700

    Rename clean_text_and_split and update docstring

--- python/practices/practice-3/src/path2/no_whitespace_punctuation_words.py
+++ python/practices/practice-3/src/path2/no_whitespace_punctuation_words.py
@@ -3,47 +3,51 @@ Reads a file, breaks each line into words, strips whitespace and
 punctuation from the words, and converts them to lowercase.
 """
 
+
 import string
 import os
 from helpers.file_reader import read_file
 from constants.constants import path_of_test_file
 
 
-def clean_word(test):
+def clean_text_and_split(test):
     """
-    Clean and process the content of a file.
+    Cleans and splits a text into words.
 
     Parameters:
-        - test (list of str): The lines of text to be cleaned.
+        test (list): A list of text lines to process.
 
     Returns:
-        - list of str: A list of cleaned words.
+        list: A list of cleaned and split words.
     """
-
     cleaned_words = []
 
     for line in test:
-        line = line.strip().split()
+        line = line.strip()
 
-        for word in line:
-            cleaned_word = ""
+        if line:
+            words = line.split()
 
-            for letter in word:
-                if letter not in string.whitespace and letter not in string.punctuation:
-                    cleaned_word += letter.lower()
+            for word in words:
+                cleaned_word = "".join(
+                    letter.lower() for letter in word if letter not in string.whitespace and letter not in string.punctuation
+                )
+                cleaned_words.append(cleaned_word)
 
-            cleaned_words.append(cleaned_word)
-
-    print(cleaned_words)
+    return cleaned_words
 
 
 def main():
     """
-    Clean and print words in file.
+    Process a text file, clean and split its content into words, and print
+    the result.
     """
 
     print(os.path.basename(__file__))
     file_content = read_file(path_of_test_file)
 
     if file_content:
-        clean_word(file_content)
+        cleaned_words = clean_text_and_split(file_content)
+
+        for word in cleaned_words:
+            print(word)
