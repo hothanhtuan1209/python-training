fa490f4f83d24ec1e36682e41c5b7affd32b8fe4
Tuan <hothanhtuan1209@gmail.com>
2023-11-05 22:51:28 +0700

    Update docstring and idiomatic for if statement

--- python/practices/practice-3/src/path2/missing_words_checker.py
+++ python/practices/practice-3/src/path2/missing_words_checker.py
@@ -13,12 +13,12 @@ def find_word_notin_list(file_ebook, file_markov):
     Find words in the ebook content that are not in the markov content.
 
     Parameters:
-        ebook_content (set): The set of words in the ebook content.
-        markov_content (set): The set of words in the markov content.
+        - file_ebook (file): The ebook content file.
+        - file_markov (file): The markov content file.
 
     Returns:
-        set: A set of words found in the ebook content but not in the markov
-        content.
+        - set: A set of words found in the ebook content but not in the
+        markov content.
     """
 
     ebook_content = set(file_ebook.read().lower().split())
@@ -39,5 +39,5 @@ def main():
     content_of_markov = read_file_content(markov_file)
     content_of_ebook = read_file_content(ebook_file)
 
-    if content_of_markov and content_of_ebook is not None:
+    if content_of_markov and content_of_ebook:
         find_word_notin_list(content_of_ebook, content_of_markov)
