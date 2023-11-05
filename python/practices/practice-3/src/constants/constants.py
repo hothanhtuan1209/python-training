d0bf187e957052710213847318ef9b69e674d278
Tuan <hothanhtuan1209@gmail.com>
2023-11-05 22:57:01 +0700

    Rename constant of file text

--- python/practices/practice-3/src/constants/constants.py
+++ python/practices/practice-3/src/constants/constants.py
@@ -1,5 +1,5 @@
 # Constant of file markov.txt
-markov_file = 'markov.txt'
+path_of_markov_file = 'markov.txt'
 
 # Constant of file ebook.txt
-ebook_file = "ebook.txt"
+path_of_ebook_file = "ebook.txt"
