8d1ad591afa1c9681d8276a39908a903dfabc816
Tuan <hothanhtuan1209@gmail.com>
2023-11-06 10:15:29 +0700

    Update helper to read content file

---
+++ python/practices/practice-3/src/helpers/file_reader.py
@@ -0,0 +1,30 @@
+"""
+This module has the function of reading and retrieving the content of a file
+"""
+
+
+def read_file(file_path):
+    """
+    Read and return the content of a text file.
+    Args:
+        file_path (str): The path to the input file to be read.
+    Returns:
+        str or None: The content of the file if successful, or None if there
+        was an error.
+    """
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+        return content
+
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+
+        return None
+
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+
+        return None
