bf3d6b6df3316dd2180c0f0c298392fef6f8b432
Tuan <hothanhtuan1209@gmail.com>
2023-11-07 14:05:17 +0700

    Update logic code for no_whitespace_punctuation_words.py

--- python/practices/practice-3/src/path2/no_whitespace_punctuation_words.py
+++ python/practices/practice-3/src/path2/no_whitespace_punctuation_words.py
@@ -4,7 +4,6 @@ punctuation from the words, and converts them to lowercase.
 """
 
 
-import string
 import os
 from helpers.file_reader import read_file
 from constants.constants import TEST
@@ -12,36 +11,28 @@ from constants.constants import TEST
 
 def clean_text_and_split(test):
     """
-    Cleans and splits a text into words.
+    Cleans and splits a text into words, removing whitespace and punctuation.
 
     Parameters:
-        test (list): A list of text lines to process.
+        test (str): A string containing the text to process.
 
     Returns:
         list: A list of cleaned and split words.
     """
 
-    cleaned_words = []
+    cleaned_text = ''.join(
+        char.lower() if char.isalnum() or char.isspace() else ' '
+        for char in test
+    )
 
-    for line in test:
-        line = line.strip()
-
-        if line:
-            words = line.split()
-
-            for word in words:
-                cleaned_word = "".join(
-                    letter.lower() for letter in word if letter not in string.whitespace and letter not in string.punctuation
-                )
-                cleaned_words.append(cleaned_word)
+    cleaned_words = [word for word in cleaned_text.split() if word]
 
     return cleaned_words
 
 
 def main():
     """
-    Process a text file, clean and split its content into words, and print
-    the result.
+    Reads a text file, cleans it, and prints the cleaned words.
     """
 
     print(os.path.basename(__file__))
@@ -49,6 +40,5 @@ def main():
 
     if file_content:
         cleaned_words = clean_text_and_split(file_content)
-
         for word in cleaned_words:
             print(word)
