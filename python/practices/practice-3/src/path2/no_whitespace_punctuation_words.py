aa7a53245226377a70292d8c4035a83ba71e53f0
Tuan <hothanhtuan1209@gmail.com>
2023-11-07 17:23:03 +0700

    Update parameter name and move if statement to clean_and_split_text function

--- python/practices/practice-3/src/path2/no_whitespace_punctuation_words.py
+++ python/practices/practice-3/src/path2/no_whitespace_punctuation_words.py
@@ -9,20 +9,23 @@ from helpers.file_reader import read_file
 from constants.constants import TEST
 
 
-def clean_and_split_text(test):
+def clean_and_split_text(content_text):
     """
     Cleans and splits a text into words, removing whitespace and punctuation.
 
-    Parameters:
-        test (str): A string containing the text to process.
+    Args:
+        content_text (str): A string containing the text to process.
 
     Returns:
         list: A list of cleaned and split words.
     """
 
+    if not content_text:
+        return []
+
     cleaned_text = ''.join(
         char.lower() if char.isalnum() or char.isspace() else ' '
-        for char in test
+        for char in content_text
     )
 
     cleaned_words = [word for word in cleaned_text.split() if word]
@@ -37,8 +40,7 @@ def main():
 
     print(os.path.basename(__file__))
     file_content = read_file(TEST)
+    cleaned_words = clean_and_split_text(file_content)
 
-    if file_content:
-        cleaned_words = clean_and_split_text(file_content)
-        for word in cleaned_words:
-            print(word)
+    for word in cleaned_words:
+        print(word)
