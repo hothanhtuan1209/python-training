450d4edfb351a763f4b37db5f3cd7c1c10ddc67a
Tuan <hothanhtuan1209@gmail.com>
2023-11-28 17:15:33 +0700

    Fix app name in urls.py

--- django/manager_employees/manager_employees/urls.py
+++ django/manager_employees/manager_employees/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path("authentication/", include("authentications.urls")),
+    path("authentication/", include("authentication.urls")),
     path('admin/', admin.site.urls),
 ]
