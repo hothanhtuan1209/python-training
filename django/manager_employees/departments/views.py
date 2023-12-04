6654553caf9fadead0a9056456ea1debce8827a0
Tuan Ho <hothanhtuan1209@gmai.com>
2023-12-04 17:05:20 +0700

    Update css for item current page

--- django/manager_employees/departments/views.py
+++ django/manager_employees/departments/views.py
@@ -9,5 +9,6 @@ def departments_list(request):
     This function get a list of departments from the database.
     """
     
+    context = {'current_page': 'departments_list'}
     departments = Department.objects.all()
-    return render(request, 'departments/departments_list.html', {'departments': departments})
+    return render(request, 'departments/departments_list.html', {'departments': departments, 'context': context})
