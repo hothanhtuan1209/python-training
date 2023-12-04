cf5fe3dcd686b6d74a2d5253f471b6960140b6ce
Tuan Ho <hothanhtuan1209@gmai.com>
2023-12-04 14:45:44 +0700

    Update url for view get list departments

--- django/manager_employees/departments/urls.py
+++ django/manager_employees/departments/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import departments_list
 
 urlpatterns = [
-    path('deparments/departments_list', departments_list, name='departments_list')
+    path('departments_list', departments_list, name='departments_list')
 ]
