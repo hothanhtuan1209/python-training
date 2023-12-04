8e24e69d226d1072ca7aeb7545d2bf030e3d9f0d
Tuan Ho <hothanhtuan1209@gmai.com>
2023-12-04 14:44:50 +0700

    Update view get list departments

--- django/manager_employees/departments/views.py
+++ django/manager_employees/departments/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from models import Department
+from .models import Department
 
 
 @login_required
@@ -10,4 +10,4 @@ def departments_list(request):
     """
     
     departments = Department.objects.all()
-    return render(request, 'departments/departments_list.html', {'departments': departments})
+    return render(request, 'departments_list.html', {'departments': departments})
