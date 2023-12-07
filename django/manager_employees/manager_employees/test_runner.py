bd66a6b774a1b77f5fcc273b0fd16f4e211f6555
Tuan Ho <hothanhtuan1209@gmail.com>
2023-12-07 09:14:05 +0700

    Remove file redundant

--- django/manager_employees/manager_employees/test_runner.py
+++
@@ -1,7 +0,0 @@
-from django.test.runner import DiscoverRunner
-
-
-class CustomTestRunner(DiscoverRunner):
-    def build_suite(self, test_labels, extra_tests=None, **kwargs):
-        test_labels += ('tests.login_test',)
-        return super().build_suite(test_labels, extra_tests, **kwargs)
