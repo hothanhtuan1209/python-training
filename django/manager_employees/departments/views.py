3d6f674cf594d14a4ae5131a162e4dd7d58186d9
Tuan Ho <hothanhtuan1209@gmail.com>
2023-12-07 17:04:28 +0700

    Update view and url to add new department

--- django/manager_employees/departments/views.py
+++ django/manager_employees/departments/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import NewDepartmentForm
 from .models import Department
 
 
@@ -13,3 +15,39 @@ def departments(request):
     return render(request, 'list.html', {
         'departments': departments, 'context': context
     })
+
+
+@login_required
+def create_department(request):
+    """
+    View function for creating a new department.
+
+    If the request method is GET, it renders the form to create a new
+    department. If the request method is POST, it processes the
+    form submission.
+    """
+
+    if request.method != 'POST':
+        form = NewDepartmentForm()
+
+    form = NewDepartmentForm(request.POST)
+
+    if form.is_valid():
+        name = form.cleaned_data["name"]
+        description = form.cleaned_data["description"]
+
+        if Department.objects.filter(name=name).exists():
+            messages.error(
+                "Department's name is already exists"
+                )
+            return redirect('create_department')
+
+        department = Department(name=name, description=description)
+        department.save()
+
+        messages.success(
+            "Create department successfully"
+        )
+        return redirect('create_department')
+
+    return render(request, 'new.html', {'form': form})
