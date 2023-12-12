14ca76356dc7fb0d7651d9fb1b84532e11a3d3c7
Tuan Ho <hothanhtuan1209@gmail.com>
2023-12-12 16:53:41 +0700

    Update view gir list departments

--- django/manager_employees/departments/views.py
+++ django/manager_employees/departments/views.py
@@ -9,6 +9,6 @@ def departments(request):
     This function get a list of departments from the database.
     """
 
-    departments = Department.objects.all().order_by("-id")[:10]
+    departments = Department.objects.all().order_by("-created_at")[:10]
     context = {"departments": departments, "current_page": "departments"}
     return render(request, "list.html", context)
