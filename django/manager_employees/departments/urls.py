2010df6144e0229f54b0c4842b4a439d92a7c198
Tuan Ho <hothanhtuan1209@gmail.com>
2023-12-14 10:59:33 +0700

    Update url for view create department

--- django/manager_employees/departments/urls.py
+++ django/manager_employees/departments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import departments
+from .views import departments, create_department
 
 urlpatterns = [
-    path('', departments, name='departments')
+    path('', departments, name='departments'),
+    path('new/', create_department, name='create_department'),
 ]
